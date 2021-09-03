66c5eb7c93d479910c42e0e5e7d18dee35504674
Viz <viz@linux.com>
2021-09-04 01:49:19 +0800

    Update provisioner.go

--- provisioner.go
+++ provisioner.go
@@ -198,7 +198,7 @@ func (p *LocalPathProvisioner) Provision(opts pvController.ProvisionOptions) (*v
 	}
 
 	name := opts.PVName
-	folderName := strings.Join([]string{name, opts.PVC.Namespace, opts.PVC.Name}, "_")
+	folderName := strings.Join([]string{opts.PVC.Namespace, opts.PVC.Name}, "_")
 
 	path := filepath.Join(basePath, folderName)
 	logrus.Infof("Creating volume %v at %v:%v", name, node.Name, path)
